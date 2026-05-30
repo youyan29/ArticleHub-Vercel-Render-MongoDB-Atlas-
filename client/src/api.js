541cc241343dde67013fe9ee32d6e4e465605899
youyan29 <2960232181@qq.com>
2026-05-30 21:27:45 +0800

    test

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
@@ -12,6 +12,16 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
+});
+
+export default api;*/
+
+import axios from "axios";
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 export default api;
